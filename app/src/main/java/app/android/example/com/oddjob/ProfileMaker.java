3f136ad7c5183e1a9a9145aeceb714c7970b9eab
Jocelyn Tran <jocelyn.tran1@gmail.com>
2016-06-25 16:26:46 -0700

    Added profileMaker layout

---
+++ app/src/main/java/app/android/example/com/oddjob/ProfileMaker.java
@@ -0,0 +1,11 @@
+package app.android.example.com.oddjob;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by jocelyn on 6/25/16.
+ */
+public class ProfileMaker extends Fragment {
+
+
+}
